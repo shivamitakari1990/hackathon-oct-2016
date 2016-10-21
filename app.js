3eb608413ce918136008f6504f514598f4341b85
shivamitakari1990 <shivananda.mitakari@timeinc.com>
2016-10-21 15:41:24 +0530

    google search support

--- app.js
+++ app.js
@@ -4,9 +4,15 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+// include google cse
+var GoogleSearch = require('google-search');
+var googleSearch = new GoogleSearch({
+  key: 'AIzaSyCET7mR5tt8IRFck489wxqrD3ButhVVjpQ',
+  cx: '002246757074746565668:zel-tatiewm'
+});
+
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 
@@ -23,7 +29,18 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
+
+app.use('/google-cse', function(req, res, next) {
+  googleSearch.build({
+    q: "golf",
+    start: 0,
+    num: 10
+  }, function(error, response) {
+    console.log(response);
+  });
+res.send('dd');
+  //res.render('google-cse', { title: 'Express' });
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
